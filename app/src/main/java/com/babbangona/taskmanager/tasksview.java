0975b603a18ae0dda808097630d8e1b641713a02
Praise <praise.okonta@babbangona.com>
2020-09-22 22:25:07 +0100

    tabs layout feature

--- app/src/main/java/com/babbangona/taskmanager/tasksview.java
+++ app/src/main/java/com/babbangona/taskmanager/tasksview.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class tasksview extends AppCompatActivity {
 
     private TextView username;
+    private MaterialButton logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +21,23 @@ public class tasksview extends AppCompatActivity {
 
         //initialize view elements
         username = findViewById(R.id.txtUsername);
+        logout = findViewById(R.id.btnLogout);
 
         //receive intent
         Intent intent = getIntent();
         String displayName = intent.getStringExtra("username");
+        String upperString = displayName.substring(0, 1).toUpperCase() + displayName.substring(1).toLowerCase();
 
         //use data received
-        username.setText(displayName);
+        username.setText(upperString + "'s" + " Tasks");
 
+    }
+
+
+    public void logout(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.putExtra("true", "true");
+        startActivity(intent);
     }
 
 }
